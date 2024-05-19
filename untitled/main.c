f276ea979031f1689a2681650632e85abe25fd7d
Baptiste BERTRAND <baptiste.bertrand44@gmail.com>
2024-05-19 22:27:12 +0200

    mise à jour des boucles de vérification dans column.h

--- untitled/main.c
+++ untitled/main.c
@@ -9,10 +9,10 @@ int main() {
     CDATAFRAME* cdtframe=create_empty_CDataframe();
     printf("%s;%d;%d;%p \n",mycol->titre,mycol->tl,mycol->tp,mycol->tab);
     int val = 5;
-
     print_col(mycol);
     column_menu(mycol);
     print_col(mycol);
+    printf(mycol->tl)
     printf("%d \n", count_occurrences(mycol,5));
     return 0;
 
