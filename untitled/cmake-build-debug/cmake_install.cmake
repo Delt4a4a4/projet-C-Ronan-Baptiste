735dd1465d48b4b31509eb7155f2b1e7bf83ecb7
Baptiste BERTRAND <baptiste.bertrand44@gmail.com>
2024-05-20 21:03:19 +0200

    ajout des menus

--- untitled/cmake-build-debug/cmake_install.cmake
+++ untitled/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/20231135/CLionProjects/projet-C-Ronan-Baptiste/untitled
+# Install script for directory: C:/Users/Baptiste BERTRAND/OneDrive - Efrei/Bureau/projet-C-Ronan-Baptiste/untitled
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/20231135/CLionProjects/projet-C-Ronan-Baptiste/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Baptiste BERTRAND/OneDrive - Efrei/Bureau/projet-C-Ronan-Baptiste/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
